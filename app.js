a5123c3ed4ef5e26534a52731917781957dc5884
Annie <awong0932@gmail.com>
2023-05-24 20:24:34 -0400

    New func playRound that returns winner

--- app.js
+++ app.js
@@ -1,18 +1,41 @@
-//function getComputerChoice return either rock paper scissors
+//return rock paper scissors
 function getComputerChoice() {
     let choice = Math.floor(Math.random()*3);
     switch (choice) {
         case 0: 
-            choice = "rock";
+            choice = "Rock";
             break;
         case 1: 
-            choice = "scissors";
+            choice = "Scissors";
             break;
         case 2:
-            choice = "paper";
+            choice = "Paper";
             break;
     }
-    return choice
+    return choice;
 }
 
-console.log(getComputerChoice());
+//returns winner
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === "Rock" && computerSelection === "Paper"){
+        return "You Lose! Paper beats Rock";
+    } else if (playerSelection === "Paper" && computerSelection === "Scissors"){
+        return "You lose! Scissors beats Paper"; 
+    } else if (playerSelection === "Scissors" && computerSelection === "Rock"){
+        return "You lose! Rock beats Scissors";
+    } else if (playerSelection === "Paper" && computerSelection === "Rock"){
+        return "You Win! Paper beats Rock";
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper"){
+        return "You Win! Scissors beats Paper";
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors"){
+        return "You Win! Rock beats Scissors";
+    } else if (playerSelection === "Rock" && computerSelection === "Rock" || (playerSelection === "Paper" && computerSelection === "Paper") || (playerSelection === "Scissors" && computerSelection === "Scissors")){
+        return "Tie!";
+    }
+}
+
+
+const playerSelection = prompt('Enter rock paper or scissors');
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
